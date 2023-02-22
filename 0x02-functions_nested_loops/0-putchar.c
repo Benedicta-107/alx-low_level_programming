c3f0862c8fcbf265d2aaa8b350e5019d03001925
Benedicta-107 <benedictaibeawuchi@gmail.com>
2023-02-22 05:20:12 -0800

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	char *sh = "Main";
+	char *sh = "_putchar";
 
 	while (*sh)
 	{

c35c9d9aa6cb4e0b7694cd3929f68eee1dba8d9e
Benedicta-107 <benedictaibeawuchi@gmail.com>
2023-02-24 19:35:57 -0800

    i6

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,36 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - prints a random int and tells sign
- *
- * This function outputs a random integer and tells
- * whether it is positive, zero or negative
- *
- * Return: Always returns 0.
- *
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
-	int n;
+	int i;
 
-	n = 0;
-	/* my code goes in there */
+	i = 0;
+	positive_or_negative(i);
 
-	if (n > 0)
-		{
-		printf("%d is positive\n", n);
-	}
-	else
-		{
-		if (n < 0)
-			{
-			printf("%d is negative\n", n);
-		}
-		else
-			{
-			printf("%d is zero\n", n);
-		}
-	}
 	return (0);
 }

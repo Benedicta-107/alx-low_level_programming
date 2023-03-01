a9c93900df61cd7f5b3e48b40b68d00acb8b3c50
Benedicta-107 <benedictaibeawuchi@gmail.com>
2023-02-28 16:17:34 -0800

    reverse string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,11 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
+#include "main.h"
 
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+/*size_t _strlen(const char *str);*/
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -14,4 +14,3 @@ void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
 
 #endif
-

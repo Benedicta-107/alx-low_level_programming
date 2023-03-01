1290fb71619979719afb02b7af20d30c2c4f6f05
Benedicta-107 <benedictaibeawuchi@gmail.com>
2023-02-28 17:10:38 -0800

    print character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char of a string
+ * @str: the string that will contain the characters
+ */
+void puts2(char *str)
+{
+	int i = 0, len = 0;
+		while (str[i + 1])
+			len++;
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}

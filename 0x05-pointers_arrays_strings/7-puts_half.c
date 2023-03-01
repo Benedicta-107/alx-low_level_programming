78e9dd31384e7623d4c19dd3f22a02d1cc3c527e
Benedicta-107 <benedictaibeawuchi@gmail.com>
2023-02-28 18:55:46 -0800

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half- it prints half of the string
+ * @str: string to be printed
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, len = 0, n;
+
+	while (str[len] != '\0')
+		len++;
+	if ((len % 2) == 0)
+	{
+		n = len / 2;
+		for (i = n; i < len; i++)
+		_putchar(str[i]);
+	}
+	else
+	{
+		n = (len - 1) / 2;
+		for (i = n - 1; i < len; i++)
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
